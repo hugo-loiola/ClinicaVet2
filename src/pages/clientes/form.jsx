a37d4f4a91dd22f09df8bc1acbea9d32c4d6e5de
Hugo Loiola <hugoloiola.1@gmail.com>
2023-06-14 15:55:02 -0300

    MACIEEEEEEL

---
+++ src/pages/clientes/form.jsx
@@ -0,0 +1,151 @@
+import Pagina from "@/components/Pagina";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { BsArrowLeftCircleFill, BsCheck2 } from "react-icons/bs";
+import { mask } from "remask";
+
+const form = () => {
+  const { push } = useRouter();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+  const [animal, setAnimal] = useState([]);
+  useEffect(() => {
+    getAll();
+  }, []);
+
+  function getAll() {
+    axios.get("/api/animais").then((res) => {
+      setAnimal(res.data);
+    });
+  }
+  function salvar(dados) {
+    axios.post("/api/clientes", dados);
+    push("/clientes");
+  }
+
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    const mascara = event.target.getAttribute("mask");
+    setValue(name, mask(value, mascara));
+  }
+
+  return (
+    <Pagina titulo="Novo Cliente">
+      <Form>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="nome">
+            <Form.Label>Nome: </Form.Label>
+            <Form.Control type="text" {...register("nome")} />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="cpf">
+            <Form.Label>CPF: </Form.Label>
+            <Form.Control
+              type="text"
+              mask="999.999.999-99"
+              {...register("cpf")}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="animal">
+            <Form.Label>Animal: </Form.Label>
+            <Form.Select {...register("animal")}>
+              {animal.map((item) => (
+                <option key={item.id}>{item.nome}</option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="email">
+            <Form.Label>Email: </Form.Label>
+            <Form.Control
+              type="email"
+              {...register("email", { maxLength: 100 })}
+            />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="telefone">
+            <Form.Label>Telefone: </Form.Label>
+            <Form.Control
+              type="tel"
+              mask="(99) 99999-9999"
+              {...register("telefone")}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="cep">
+            <Form.Label>CEP: </Form.Label>
+            <Form.Control
+              type="text"
+              mask="99999-999"
+              {...register("cep")}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="logradouro">
+            <Form.Label>Logradouro: </Form.Label>
+            <Form.Control
+              type="text"
+              {...register("logradouro", { maxLength: 100 })}
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="complemento">
+            <Form.Label>Complemento: </Form.Label>
+            <Form.Control
+              type="text"
+              {...register("complemento", { maxLength: 100 })}
+            />
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="numero">
+            <Form.Label>Numero: </Form.Label>
+            <Form.Control
+              type="text"
+              {...register("numero", { maxLength: 20 })}
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="bairro">
+            <Form.Label>Bairro: </Form.Label>
+            <Form.Control
+              type="text"
+              {...register("bairro", { maxLength: 100 })}
+            />
+          </Form.Group>
+        </Row>
+
+        <div className="text-center">
+          <Button variant="success" onClick={handleSubmit(salvar)}>
+            <BsCheck2 className="me-1" />
+            Salvar
+          </Button>
+          <Link href={"/alunos"} className="ms-2 btn btn-danger">
+            <BsArrowLeftCircleFill className="me-1" />
+            Voltar
+          </Link>
+        </div>
+      </Form>
+    </Pagina>
+  );
+};
+
+export default form;
--- src/pages/clientes/index.jsx
+++ src/pages/clientes/index.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import MeuCard from "@/components/MeuCard";
+import Pagina from "@/components/Pagina";
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { Card, Col, Row, Table } from "react-bootstrap";
+import { BsPlusCircle } from "react-icons/bs";
 
 const index = () => {
-  return <div>index</div>;
+  const [clientes, setClientes] = useState([]);
+
+  useEffect(() => {
+    getAll();
+  }, []);
+
+  function getAll() {
+    axios.get("/api/clientes").then((res) => {
+      setClientes(res.data);
+    });
+  }
+
+  return (
+    <Pagina titulo="Clientes">
+      <Link href={"/clientes/form"} className="btn btn-primary mb-2">
+        Novo
+        <BsPlusCircle className="ms-1" />
+      </Link>
+      <Row>
+        {clientes.map((item) => (
+          <Col key={item.id}>
+            <Link
+              href={`/clientes/${item.id}`}
+              style={{
+                textDecoration: "none",
+                color: "black",
+                textAlign: "center",
+              }}
+            >
+              <MeuCard>
+                <Card.Img src={item.foto} />
+                <Card.Body>
+                  <Card.Title>{item.nome}</Card.Title>
+                </Card.Body>
+              </MeuCard>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </Pagina>
+  );
 };
 
 export default index;
