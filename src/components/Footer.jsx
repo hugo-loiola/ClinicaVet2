cf0d0391a0ac0b45fae084361406b5536ec9c885
Hugo Loiola <hugoloiola.1@gmail.com>
2023-06-15 16:25:43 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { MdOutlineMail, MdPlace, MdWhatsapp } from "react-icons/md";
 
 const Footer = () => {
   return (
@@ -7,23 +9,40 @@ const Footer = () => {
       fluid
       style={{
         width: "100%",
-        borderTop: "5px solid orange",
+        backgroundColor: "orange",
       }}
       className="bottom-0 py-3 text-center"
     >
       <Row>
-        <Col md={3}>
-          <h3>Nos encontre</h3>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3839.1536184503902!2d-48.12616322396138!3d-15.795851884844724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935bcb8115bc6165%3A0x9ddf0cf83dbc3b8a!2sSt.%20O%20Qno%203%20Conjunto%20H%20-%20St.%20O%20QNO%203%20Conj.%20H%20-%20Ceil%C3%A2ndia%2C%20Bras%C3%ADlia%20-%20DF%2C%2072250-308!5e0!3m2!1spt-BR!2sbr!4v1686759734605!5m2!1spt-BR!2sbr"
-            width="400"
-            height="300"
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+        <Col>
+          <h3 className="mb-3">Nos encontre</h3>
+          <Link
+            href="https://www.google.com/maps/dir//Centro%20Universit%C3%A1rio%20IESB%20-%20Campus%20Ceil%C3%A2ndia"
+            target="_blank"
+            style={{ color: "black" }}
+          >
+            <MdPlace size={"50"} />
+          </Link>
+          <Col className="mt-3">
+            Ceilândia Norte QNN 31 Distrito Federal, Brasília - DF, 72225-315
+          </Col>
         </Col>
-        <Col md={6}></Col>
+        <Col>
+          <h4>AMEVET | CLÍNICA VETERINÁRIA </h4>Amor e Dedicação para seu melhor
+          amigo… Aguardamos a sua visita!
+        </Col>
+        <Col>
+          <h3>Converse Conosco</h3>
+          <a href="https://wa.me/5561991862235">
+            <MdWhatsapp size={"25"} /> (61) 99186-2235
+          </a>
+          <p>
+            <MdOutlineMail size={"25"} /> amevet@gmail.com
+          </p>
+          <a href="mailto:hugoqueiroz2412@gmail.com?subject=Conversa">
+            Enviar email
+          </a>
+        </Col>
       </Row>
       <p>
         Todos os direitos reservados® Feito por{" "}
