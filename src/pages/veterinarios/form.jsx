b18e432c0b1217b5684121546ff05724f1ab8169
Hugo Loiola <hugoloiola.1@gmail.com>
2023-06-15 13:56:18 -0300

    veterinarios

---
+++ src/pages/veterinarios/form.jsx
@@ -0,0 +1,154 @@
+import Pagina from "@/components/Pagina";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { BsArrowLeftCircleFill, BsCheck2 } from "react-icons/bs";
+import { mask } from "remask";
+
+const form = () => {
+  const { push } = useRouter();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+  const [animal, setAnimal] = useState([]);
+
+  function salvar(dados) {
+    axios.post("/api/veterinarios", dados);
+    push("/veterinarios");
+  }
+
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    const mascara = event.target.getAttribute("mask");
+    setValue(name, mask(value, mascara));
+  }
+
+  return (
+    <Pagina titulo="Novo Cliente">
+      <Form>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="nome">
+            <Form.Label>Nome: </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Coloque seu Nome"
+              {...register("nome")}
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="cpf">
+            <Form.Label>CPF: </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="123.456.789.09"
+              mask="999.999.999-99"
+              {...register("cpf")}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="salario">
+            <Form.Label>Salario </Form.Label>
+            <Form.Control type="text" {...register("salario")} />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="email">
+            <Form.Label>Email: </Form.Label>
+            <Form.Control
+              placeholder="exemplo@teste.com"
+              type="email"
+              {...register("email", { maxLength: 100 })}
+            />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="telefone">
+            <Form.Label>Telefone: </Form.Label>
+            <Form.Control
+              type="tel"
+              placeholder="(01) 23456-78909"
+              mask="(99) 99999-9999"
+              {...register("telefone")}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="cep">
+            <Form.Label>CEP: </Form.Label>
+            <Form.Control
+              placeholder="12345-678"
+              type="text"
+              mask="99999-999"
+              {...register("cep")}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="logradouro">
+            <Form.Label>Logradouro: </Form.Label>
+            <Form.Control
+              type="text"
+              {...register("logradouro", { maxLength: 100 })}
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="complemento">
+            <Form.Label>Complemento: </Form.Label>
+            <Form.Control
+              type="text"
+              {...register("complemento", { maxLength: 100 })}
+            />
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="numero">
+            <Form.Label>Número: </Form.Label>
+            <Form.Control
+              type="text"
+              mask="999"
+              {...register("numero")}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="bairro">
+            <Form.Label>Bairro: </Form.Label>
+            <Form.Control type="text" {...register("bairro")} />
+          </Form.Group>
+        </Row>
+        <Form.Group controlId="foto" className="mb-3">
+          <Form.Label>Foto: </Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Coloque sua Imagem"
+            {...register("foto")}
+          />
+        </Form.Group>
+
+        <div className="text-center">
+          <Button variant="success" onClick={handleSubmit(salvar)}>
+            <BsCheck2 className="me-1" />
+            Salvar
+          </Button>
+          <Link href={"/alunos"} className="ms-2 btn btn-danger">
+            <BsArrowLeftCircleFill className="me-1" />
+            Voltar
+          </Link>
+        </div>
+      </Form>
+    </Pagina>
+  );
+};
+
+export default form;
--- src/pages/veterinarios/index.jsx
+++ src/pages/veterinarios/index.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import MeuCard from "@/components/MeuCard";
+import Pagina from "@/components/Pagina";
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { Card, Col, Row, Table } from "react-bootstrap";
+import { BsPlusCircle } from "react-icons/bs";
 
 const index = () => {
-  return <div>index</div>;
+  const [veterinarios, setClientes] = useState([]);
+
+  useEffect(() => {
+    getAll();
+  }, []);
+
+  function getAll() {
+    axios.get("/api/veterinarios").then((res) => {
+      setClientes(res.data);
+    });
+  }
+
+  return (
+    <Pagina titulo="Veterinarios">
+      <Link href={"/veterinarios/form"} className="btn btn-primary mb-2">
+        Novo
+        <BsPlusCircle className="ms-1" />
+      </Link>
+      <Row>
+        {veterinarios.map((item) => (
+          <Col key={item.id}>
+            <Link
+              href={`/veterinarios/${item.id}`}
+              style={{
+                textDecoration: "none",
+                color: "black",
+                textAlign: "center",
+              }}
+            >
+              <MeuCard>
+                <Card.Img src={item.foto} />
+                <Card.Body>
+                  <Card.Title>{item.nome}</Card.Title>
+                </Card.Body>
+              </MeuCard>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </Pagina>
+  );
 };
 
 export default index;
